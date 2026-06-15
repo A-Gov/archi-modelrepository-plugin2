A-Gov/archi-modelrepository-plugin2

commit 412bf2055bf4d0283d5b24e5a1534bb913758751

Load RepositoryTreeModel backing file when creating instance not in ModelRepositoryTreeViewer
- The RepositoryTreeModel and its backing file can be accessed when the ModelRepositoryView is not open or not created so load the backing file when first accessed and don't dispose it

com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/treemodel/RepositoryTreeModel.java
@@ -8,9 +8,10 @@ package com.archimatetool.modelrepository.treemodel;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -28,6 +29,8 @@ import com.archimatetool.modelrepository.repository.RepoUtils;
  */
 public class RepositoryTreeModel extends Group {
     
+    private static Logger logger = Logger.getLogger(RepositoryTreeModel.class.getName());
+
     // Can be set to false for testing
     static boolean saveToManifest = true;
     
@@ -54,6 +57,14 @@ public class RepositoryTreeModel extends Group {
     
     private RepositoryTreeModel() {
         super(null);
+        
+        try {
+            loadBackingFile();
+        }
+        catch(IOException | JDOMException ex) {
+            ex.printStackTrace();
+            logger.log(Level.SEVERE, "Loading Backing File", ex); //$NON-NLS-1$
+        }
     }
 
     /**
@@ -85,10 +96,7 @@ public class RepositoryTreeModel extends Group {
         return Optional.empty();
     }
     
-    public void loadManifest() throws IOException, JDOMException {
-        groups = new ArrayList<>();
-        repos = new ArrayList<>();
-        
+    private void loadBackingFile() throws IOException, JDOMException {
         if(backingFile.exists()) {
             Document doc = JDOMUtils.readXMLFile(backingFile);
             if(doc.hasRootElement()) {
@@ -185,12 +193,5 @@ public class RepositoryTreeModel extends Group {
                 listener.treeEntryChanged(entry);
             }
         }
-    }
-    
-    public void dispose() {
-        listeners = null;
-        groups = null;
-        repos = null;
-        instance = null;
     }
 }

com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/views/repositories/ModelRepositoryTreeViewer.java
@@ -33,14 +33,13 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.TreeItem;
-import org.jdom2.JDOMException;
 
 import com.archimatetool.editor.ui.components.TreeTextCellEditor;
 import com.archimatetool.editor.utils.StringUtils;
 import com.archimatetool.modelrepository.IModelRepositoryImages;
 import com.archimatetool.modelrepository.repository.BranchInfo;
-import com.archimatetool.modelrepository.repository.GitUtils;
 import com.archimatetool.modelrepository.repository.BranchInfo.Option;
+import com.archimatetool.modelrepository.repository.GitUtils;
 import com.archimatetool.modelrepository.repository.IArchiRepository;
 import com.archimatetool.modelrepository.repository.IRepositoryListener;
 import com.archimatetool.modelrepository.repository.RepositoryListenerManager;
@@ -86,10 +85,9 @@ public class ModelRepositoryTreeViewer extends TreeViewer implements IRepository
         
         RepositoryListenerManager.getInstance().addListener(this);
         
-        // Dispose of this and clean up
+        // Remove listener
         getTree().addDisposeListener(e -> {
             RepositoryListenerManager.getInstance().removeListener(ModelRepositoryTreeViewer.this);
-            RepositoryTreeModel.getInstance().dispose();
         });
         
         // Tooltip support
@@ -165,14 +163,6 @@ public class ModelRepositoryTreeViewer extends TreeViewer implements IRepository
                 return element instanceof Group;
             }
         });
-        
-        try {
-            RepositoryTreeModel.getInstance().loadManifest();
-        }
-        catch(IOException | JDOMException ex) {
-            ex.printStackTrace();
-            logger.log(Level.SEVERE, "Loading Manifest", ex); //$NON-NLS-1$
-        }
         
         RepositoryTreeModel.getInstance().addListener(this);
         

commit 5617d8e6a2ee5dd5dc964da9d3fb7a5b2748fb92

Handle user repository folder changes by resetting tree model and recreating logger
- When user changes the default repo folder location reset the RepositoryTreeModel and FileLogger to the new location

- The file location is not stored statically

- Also ensure that file.canWrite() is not called in ModelRepositoryPlugin.getUserModelRepositoryFolder() as this will return false if the folder doesn't exist

- Also fix a regression in 412bf2055bf4d0283d5b24e5a1534bb913758751 where the RepositoryTreeModel didn't remove itself as a listener. This can cause Exception if the ModelRepositoryTreeViewer is closed and re-opened

com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/ModelRepositoryPlugin.java
@@ -18,6 +18,7 @@ import org.osgi.framework.BundleContext;
 import com.archimatetool.editor.FileLogger;
 import com.archimatetool.editor.utils.StringUtils;
 import com.archimatetool.modelrepository.preferences.IPreferenceConstants;
+import com.archimatetool.modelrepository.treemodel.RepositoryTreeModel;
 
 
 
@@ -38,6 +39,8 @@ public class ModelRepositoryPlugin extends AbstractUIPlugin {
     // The shared instance
     private static ModelRepositoryPlugin instance;
     
+    private FileLogger fileLogger;
+    
     /**
      * @return the shared instance
      */
@@ -57,8 +60,27 @@ public class ModelRepositoryPlugin extends AbstractUIPlugin {
         setSystemProperties();
         
         // Start logging
+        createLogger();
+        
+        // If user changes repo folder location, close old logger and create a new one and reset RepositoryTreeModel
+        getPreferenceStore().addPropertyChangeListener(event -> {
+            if(IPreferenceConstants.PREFS_REPOSITORY_FOLDER.equals(event.getProperty())) {
+                if(fileLogger != null) {
+                    fileLogger.close();
+                    createLogger();
+                }
+
+                RepositoryTreeModel.getInstance().reset();
+            }
+        });
+    }
+    
+    /**
+     * Create and register a new FileLogger
+     */
+    private void createLogger() {
         try {
-            FileLogger.create("com.archimatetool.modelrepository",
+            fileLogger = FileLogger.create("com.archimatetool.modelrepository",
                               getBundle().getEntry("logging.properties"),
                               new File(getUserModelRepositoryFolder(), "log-%g.txt"));
         }
@@ -99,10 +121,7 @@ public class ModelRepositoryPlugin extends AbstractUIPlugin {
         String path = getPreferenceStore().getString(IPreferenceConstants.PREFS_REPOSITORY_FOLDER);
         
         if(StringUtils.isSet(path)) {
-            File file = normalizedFile(path);
-            if(file.canWrite()) {
-                return file;
-            }
+            return normalizedFile(path);
         }
         
         // Default

com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/treemodel/RepositoryTreeModel.java
@@ -35,11 +35,6 @@ public class RepositoryTreeModel extends Group {
     static boolean saveToManifest = true;
     
     /**
-     * Backing File
-     */
-    private File backingFile = new File(ModelRepositoryPlugin.getInstance().getUserModelRepositoryFolder(), "repositories.xml"); //$NON-NLS-1$
-    
-    /**
      * Listeners
      */
     private CopyOnWriteArrayList<IRepositoryTreeModelListener> listeners = new CopyOnWriteArrayList<>(); // Avoid possible CMEs
@@ -96,13 +91,38 @@ public class RepositoryTreeModel extends Group {
         return Optional.empty();
     }
     
+    /**
+     * Reset this and load the manifest again notifying listeners
+     */
+    public void reset() {
+        groups.clear();
+        repos.clear();
+
+        try {
+            loadManifest();
+        }
+        catch(IOException | JDOMException ex) {
+            ex.printStackTrace();
+            logger.log(Level.SEVERE, "Loading Manifest", ex); //$NON-NLS-1$
+        }
+        finally {
+            fireListenerEvent(this);
+        }
+    }
+    
     private void loadManifest() throws IOException, JDOMException {
-        if(backingFile.exists()) {
+        File backingFile = getBackingFile();
+        
+        if(backingFile .exists()) {
             Document doc = JDOMUtils.readXMLFile(backingFile);
             if(doc.hasRootElement()) {
                 load(doc.getRootElement(), this);
             }
         }
+    }
+    
+    private File getBackingFile() {
+        return new File(ModelRepositoryPlugin.getInstance().getUserModelRepositoryFolder(), "repositories.xml"); //$NON-NLS-1$
     }
     
     private void load(Element parentElement, Group parentGroup) {
@@ -145,7 +165,7 @@ public class RepositoryTreeModel extends Group {
         
         save(rootElement, this);
         
-        JDOMUtils.write2XMLFile(doc, backingFile);
+        JDOMUtils.write2XMLFile(doc, getBackingFile());
     }
     
     private void save(Element parentElement, Group parentGroup) {

com.archimatetool.modelrepository/src/com/archimatetool/modelrepository/views/repositories/ModelRepositoryTreeViewer.java
@@ -85,9 +85,10 @@ public class ModelRepositoryTreeViewer extends TreeViewer implements IRepository
         
         RepositoryListenerManager.getInstance().addListener(this);
         
-        // Remove listener
+        // Remove listeners
         getTree().addDisposeListener(e -> {
             RepositoryListenerManager.getInstance().removeListener(ModelRepositoryTreeViewer.this);
+            RepositoryTreeModel.getInstance().removeListener(ModelRepositoryTreeViewer.this);
         });
         
         // Tooltip support